fb2ef0e6b4b1f2656f2c03fcdf61c9c81cae71dc
Samuel <samueldesta323@gmail.com>
2023-02-14 17:04:47 +0300

    added file 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ updates all documents' topics based on the name provided
 
 def update_topics(mongo_collection, name, topics):
     """updates all documents in mongo_collection based on name"""
-    mongo_collection.update_all({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
