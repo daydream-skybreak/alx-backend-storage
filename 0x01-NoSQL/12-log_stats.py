5c43bd416ce02c25f0e6c1258d7ef0db8d6cd736
Samuel <samueldesta323@gmail.com>
2023-02-17 22:01:13 +0300

    module not executed when imported

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """provides some stats about Nginx log stored in MongoDB"""
-import pymongo
-from pymongo import MongoClient
+if __name__ == "__main__":
+    import pymongo
+    from pymongo import MongoClient
 
+    client = MongoClient("mongodb://localhost:27017")
+    logs = client["logs"]
+    nginx = logs.nginx
 
-client = MongoClient("mongodb://localhost:27017")
-logs = client["logs"]
-nginx = logs.nginx
+    total = nginx.count_documents({})
+    print(f"{total} logs")
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-total = nginx.count_documents({})
-print(f"{total} logs")
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-for i in method:
-    print(f"\tmethod {i}: {nginx.count_documents({'method': i})}")
+    for i in method:
+        print(f"\tmethod {i}: {nginx.count_documents({'method': i})}")
